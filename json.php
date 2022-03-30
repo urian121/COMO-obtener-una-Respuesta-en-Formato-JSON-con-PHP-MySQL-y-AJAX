41ecfc6e076c5839cdc393b39a5b02ecb84221a3
Urian <urian1213viera@gmail.com>
2022-03-30 14:22:19 -0500

    Proyecto funcionar

---
+++ json.php
@@ -0,0 +1,25 @@
+<?php
+$errors = [];
+$data = [];
+
+if (empty($_POST['name'])) {
+    $errors['name'] = 'Name is required.';
+}
+
+if (empty($_POST['email'])) {
+    $errors['email'] = 'Email is required.';
+}
+
+if (empty($_POST['superheroAlias'])) {
+    $errors['superheroAlias'] = 'Superhero alias is required.';
+}
+
+if (!empty($errors)) {
+    $data['success'] = false;
+    $data['errors'] = $errors;
+} else {
+    $data['success'] = true;
+    $data['message'] = 'Success!';
+}
+
+echo json_encode($data);
